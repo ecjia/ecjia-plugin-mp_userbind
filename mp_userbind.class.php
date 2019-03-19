af77c5d5885a365dac1149a50fdb37d2a0c56e47
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-19 17:51:31 +0800

    mp、withdraw、sns插件 控制器和模板语言包升级

--- mp_userbind.class.php
+++ mp_userbind.class.php
@@ -146,8 +146,8 @@ class mp_userbind extends PlatformAbstract
             $username = RC_DB::TABLE('users')->where('user_id', $userid)->pluck('user_name');
 
             $content = [
-                'title' => '已绑定',
-                'description' => '您已拥有帐号，用户名为【'.$username.'】点击该链接可进入用户中心哦',
+                'title' => __('已绑定', 'mp_userbind'),
+                'description' => sprintf(__('您已拥有帐号，用户名为【%s】点击该链接可进入用户中心哦', 'mp_userbind'), $username),
                 'url' => RC_Uri::url('wechat/mobile_profile/init', array('openid' => $openid, 'uuid' => $uuid)),
                 'image' => RC_Plugin::plugin_dir_url(__FILE__) . '/images/wechat_thumb_userbind.png',
             ];
@@ -155,8 +155,8 @@ class mp_userbind extends PlatformAbstract
         //未绑定用户
         else {
             $content = [
-                'title' => '未绑定',
-                'description' => '抱歉，目前您还未进行账号绑定，需点击该链接进行绑定操作',
+                'title' => __('未绑定', 'mp_userbind'),
+                'description' => __('抱歉，目前您还未进行账号绑定，需点击该链接进行绑定操作', 'mp_userbind'),
                 'url' => RC_Uri::url('wechat/mobile_userbind/init',array('openid' => $openid, 'uuid' => $uuid)),
                 'image' => RC_Plugin::plugin_dir_url(__FILE__) . '/images/wechat_thumb_userbind.png',
             ];
@@ -198,7 +198,7 @@ class mp_userbind extends PlatformAbstract
             //获取用户名
             $username = RC_DB::TABLE('users')->where('user_id', $userid)->pluck('user_name');
             $content = [
-                'title' => '已绑定',
+                'title' => __('已绑定', 'mp_userbind'),
                 'description' => '您已拥有帐号，用户名为【'.$username.'】点击该链接可进入用户中心哦',
                 'url' => RC_Uri::url('wechat/mobile_profile/init', array('openid' => $openid, 'uuid' => $uuid)),
                 'image' => RC_Plugin::plugin_dir_url(__FILE__) . '/images/wechat_thumb_userbind.png',
@@ -207,8 +207,8 @@ class mp_userbind extends PlatformAbstract
         //未绑定用户
         else {
             $content = [
-                'title' => '未绑定',
-                'description' => '抱歉，目前您还未进行账号绑定，需点击该链接进行绑定操作',
+                'title' => __('未绑定', 'mp_userbind'),
+                'description' => __('抱歉，目前您还未进行账号绑定，需点击该链接进行绑定操作', 'mp_userbind'),
                 'url' => RC_Uri::url('wechat/mobile_userbind/init',array('openid' => $openid, 'uuid' => $uuid)),
                 'image' => RC_Plugin::plugin_dir_url(__FILE__) . '/images/wechat_thumb_userbind.png',
             ];
